7eab179adeeac9e71a031e389f1615a8212c3855
Henrique Jordão <>
2024-09-27 01:10:55 -0300

    Add point free project

---
+++ Projects/PointFree/PointFree/PointFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointFreeApp.swift
+//  PointFree
+//
+//  Created by Henrique Jordão on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PointFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
