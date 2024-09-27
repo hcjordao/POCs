6e677831e2094898ed04804d8ee332dfaf5536c6
Henrique Jordão <>
2024-09-27 01:09:16 -0300

    Some updates

--- Pointfree.playground/Sources/Utils.swift
+++ Pointfree.playground/Sources/Utils.swift
@@ -46,3 +46,26 @@ public func incr(_ x: Int) -> Int {
 public func square(_ x: Int) -> Int {
     return x * x
 }
+
+// MARK: - #2 Side Effects
+precedencegroup SingleTypeComposition {
+    associativity: left
+    higherThan: ForwardApplication
+}
+
+// Diamond Operator
+infix operator <>: SingleTypeComposition
+
+public func <> <A>(f: @escaping (A) -> A, g: @escaping (A) -> A) -> (A) -> A {
+    return f >>> g
+}
+
+public func <> <A>(
+    f: @escaping (inout A) -> Void,
+    g: @escaping (inout A) -> Void
+) -> (inout A) -> Void {
+    return { a in
+        f(&a)
+        g(&a)
+    }
+}
